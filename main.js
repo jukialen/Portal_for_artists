c4ad3252da524a40e6eb5fb805b170684db93a9d
jukialen <57296419+jukialen@users.noreply.github.com>
2021-01-26 18:59:51 +0100

    Created a dynamic change in the menu

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 
 const darkMode = document.querySelector('.darkMode');
 const wholePage = document.querySelector('.wholePage');
-const aPropertyAll = document.getElementsByTagName('a');
-
-const leftArrow = document.getElementsByClassName('leftArrow');
-const rightArrow = document.getElementsByClassName('rightArrow');
+// const aPropertyAll = document.getElementsByTagName('a');
+//
+// const leftArrow = document.getElementsByClassName('leftArrow');
+// const rightArrow = document.getElementsByClassName('rightArrow');
 
 changeMode = () => {
     darkMode.classList.toggle('lightMode');
@@ -41,21 +41,88 @@ const showMenu = () => nav.classList.toggle('activeMenu');
 hamburgerMenu.addEventListener('click', showMenu);
 
 
-//DYNAMIC SHOW LI IN NAV
+//REPLACE LI MENU AND REMOVE FORM LOGIN AND REGISTRATION
 
-// const account = document.querySelector('.account');
-// const logOut = document.querySelector('.logOut');
-//
-//
-// const changeNavMenu = () => {
-//     // signIn.innerHTML = '<li class="account, menu"><a href="/Portal_for_artists/PL/account.html">Konto</a></li>';
-//     // signOut.innerHTML = '<li class="logOut, menu"><a href="#">Wyloguj się</a></li>';
-//     signOut.remove();
-// };
-//
-// loginSubmit.addEventListener('click', changeNavMenu);
-//
-// createAccountSubmit.addEventListener('click', changeNavMenu);
+const ulNavbar = document.querySelector('nav ul');
+
+
+function replaceMenu() {
+    if (signIn.classList.contains('signIn') && signOut.classList.contains('signOut')) {
+        const accountNavbar = document.createElement('li');
+        accountNavbar.classList.add('menu');
+        ulNavbar.appendChild(accountNavbar);
+
+        const linkAccount = document.createElement('a');
+        linkAccount.classList.add('account');
+        linkAccount.setAttribute('href', 'account.html');
+        linkAccount.textContent = 'Konto';
+        accountNavbar.appendChild(linkAccount);
+
+        const logOutNavbar = document.createElement('li');
+        logOutNavbar.classList.add('menu');
+        ulNavbar.appendChild(logOutNavbar);
+
+        const linkLogOut = document.createElement('a');
+        linkLogOut.classList.add('logOut');
+        linkLogOut.setAttribute('href', '#');
+        linkLogOut.textContent = 'Wyloguj';
+        logOutNavbar.appendChild(linkLogOut);
+
+        signIn.parentElement.remove();
+        signOut.parentElement.remove();
+
+        login.style.display = 'none';
+        createAccount.style.display = 'none';
+    }
+}
+
+
+createAccount.addEventListener('click', replaceMenu);
+login.addEventListener('click', replaceMenu);
+
+//LOGOFF MENU
+
+const linkAccount = document.querySelector('.account');
+const linkLogOut = document.querySelector('.logOut');
+
+function logoff() {
+    if (linkAccount.classList.contains('account') && linkLogOut.classList.contains('logOut')) {
+
+        const signInNavbar = document.createElement('li');
+        signInNavbar.classList.add('menu');
+        ulNavbar.appendChild(signInNavbar);
+
+        const linkSignIn = document.createElement('a');
+        linkSighIn.classList.add('signIn');
+        linkSighIn.setAttribute('href', '#');
+        signIn.textContent = 'Zaloguj się';
+        signInNavbar.appendChild(linkSignIn);
+
+        const signOutNavbar = document.createElement('li');
+        signOutNavbar.classList.add('menu');
+        ulNavbar.appendChild(signOutNavbar);
+
+        const linkSignOut = document.createElement('a');
+        linkSighOut.classList.add('signIn');
+        linkSighOut.setAttribute('href', '#');
+        signOut.textContent = 'Zaloguj się';
+        signOutNavbar.appendChild(linkSignOut);
+
+        linkAccount.parentElement.remove();
+        linkLogOut.parentElement.remove();
+
+        login.style.display = 'grid';
+        createAccount.style.display = 'grid';
+
+    }
+}
+
+const linkSighIn = document.querySelector('.signIn');
+const linkSighOut = document.querySelector('.signOut');
+
+
+linkLogOut.addEventListener('click', logoff);
+
 
 //SHOW AND CHANGE LANGUAGE
 
