a956be9e1e7fac0470d1d637a40989a12f27caf6
jukialen <57296419+jukialen@users.noreply.github.com>
2021-07-24 21:08:33 +0200

    Adding show/hide AffixButton when scrolling

--- src/components/molecules/AffixButton/AffixButton.js
+++ src/components/molecules/AffixButton/AffixButton.js
@@ -5,22 +5,29 @@ import './AffixButton.scss';
 import { Affix, Button } from 'antd';
 import { UpOutlined } from '@ant-design/icons';
 
-import { useWindowSize } from 'hooks/useWindowSize';
-
 export const AffixButton = () => {
   const [bottom, setBottom] = useState(10);
-  const ref = useRef()
 
-  const { height, width } = useWindowSize();
+    const [visible, setVisible] = useState(false)
 
-  console.log(height)
+    const toggleVisible = () => {
+        const scrolled = document.documentElement.scrollTop;
+        if (scrolled > 300){
+            setVisible(true)
+        }
+        else if (scrolled <= 300){
+            setVisible(false)
+        }
+    };
+
+
+    window.addEventListener('scroll', toggleVisible);
 
   return (
     <Affix offsetBottom={bottom}>
       <Button type="primary" href="#top__menu" onClick={() => setBottom(10)}>
         <UpOutlined
-          ref={ref}
-          className={`up ${height < width && window.scrollY < 300 ? null : 'up__active'}`}
+          className={`up ${visible ? 'up__active' : null }`}
           aria-label="top of page button"
         />
       </Button>
