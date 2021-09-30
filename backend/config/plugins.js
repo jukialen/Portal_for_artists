0c9a6470ba9b25221d1411bee0ab642805b4e86e
jukialen <57296419+jukialen@users.noreply.github.com>
2021-09-30 19:31:30 +0200

    Editing plugins.js | Uninstall strapi nodemailer

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,19 +1,19 @@
 module.exports = ({ env }) => ({
   email: {
-    provider: env('EMAIL_PROVIDER'),
+    provider: process.env.EMAIL_PROVIDER,
     providerOptions: {
-      host: ('EMAIL_SMTP_HOST'),
-      port: env('EMAIL_SMTP_PORT'),
+      host: process.env.EMAIL_SMTP_HOST,
+      port: process.env.EMAIL_SMTP_PORT,
       //secure: true,
-      username: env('EMAIL_SMTP_USER'),
-      password: env('EMAIL_SMTP_PASS'),
+      username: process.env.EMAIL_SMTP_USER,
+      password: process.env.EMAIL_SMTP_PASS,
       //rejectUnauthorized: false,
       //requireTLS: false,
       connectionTimeout: 3,
     },
     settings: {
-      from: env('EMAIL_ADDRESS_FROM'),
-      replyTo: env('EMAIL_ADDRESS_REPLY'),
+      from: process.env.EMAIL_ADDRESS_FROM,
+      replyTo: process.env.EMAIL_ADDRESS_REPLY,
     },
   },
   // ...
